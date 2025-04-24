9892a7e87a9c793203c82cb7d0684fc05d2c461f
Ali Shan <alishanamin125@gmail.com>
2025-04-24 18:29:50 +0500

    react6

--- react6/src/Model.jsx
+++ react6/src/Model.jsx
@@ -1,39 +1,38 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 
-const Model = ({showModel , onClose}) => {
+const Model = ({ showModel, onClose }) => {
+  const eventTarget = useRef(null);
 
-    const eventTarget = useRef(null)
-
-useEffect(()=>{
-    function handleClickOutside(event){
-        if(eventTarget.current && !eventTarget.current.contains(event.target)){
-            onClose()
-        }
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (eventTarget.current && !eventTarget.current.contains(event.target)) {
+        onClose();
+      }
     }
-    window.addEventListener('mousedown' , handleClickOutside)
+    window.addEventListener("mousedown", handleClickOutside);
 
-    return ()=>{
-        window.removeEventListener('mousedown' , handleClickOutside)
-    }
-},[showModel , onClose])
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showModel, onClose]);
 
-
-
-
-
-if(showModel === false) return null
+  if (showModel === false) return null;
   return (
-    <div className='fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center p-6'>
-        <div ref={eventTarget}  className='bg-blue-400 w-[400px]  p-2  rounded  '>
-            <h1>Model</h1>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo a ea, esse harum quisquam temporibus nam maiores delectus deserunt illo dolorem, qui ipsam rem debitis nobis sunt nisi asperiores quas!
-            </p>
+    <div className="fixed top-0 left-0 w-full h-full bg-black/50 flex justify-center items-center p-6">
+      <div ref={eventTarget} className="bg-blue-400 w-[400px]  p-2  rounded  ">
+        <h1>Model</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo a ea,
+          esse harum quisquam temporibus nam maiores delectus deserunt illo
+          dolorem, qui ipsam rem debitis nobis sunt nisi asperiores quas!
+        </p>
 
-            <button onClick={onClose} className="btn btn-neutral mt-3">Close</button>
-        </div>
+        <button onClick={onClose} className="btn btn-neutral mt-3">
+          Close
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Model
+export default Model;
