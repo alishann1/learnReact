7315c8e7bd690e220679cf12cdfe99c31a2313d9
Ali Shan <alishanamin125@gmail.com>
2025-05-01 17:25:58 +0500

    remove

--- react7/src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function About() {
-  return <div className="w-screen bg-gray-500 text-5xl">About</div>;
-}
-
-export default About;
--- react7/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <div className="w-screen bg-gray-500 text-5xl">Home</div>;
-}
-
-export default Home;
